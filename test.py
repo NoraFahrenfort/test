58ce2c1d950260af9befe2ade2927c7e16723a17
NoraFahrenfort <nora.fahrenfort@tno.nl>
2017-11-09 13:57:11 +0100

    added circumference

--- test.py
+++ test.py
@@ -2,4 +2,12 @@ print('Hello World of Quantum')
 
 print("This is a change")
 
+import numpy as np
+
+def circumference(x):
+	l = np.pi*2*x
+	return l
+
+print("The circumference is", circumference(3))
+
 
